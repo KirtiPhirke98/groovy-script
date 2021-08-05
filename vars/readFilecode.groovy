14215b2276f5c6167eb3c2bc73a1299baf236bdd
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-08-05 13:28:21 +0530

    Update readFilecode.groovy

--- vars/readFilecode.groovy
+++ vars/readFilecode.groovy
@@ -3,6 +3,6 @@
 
 def call(string name) {
   echo "Hii ${name}"
-  new File("https://github.com/KirtiPhirke98/file/blob/1bd64515b0cf031528b4a01b4509ef9f725b2896/vars/Hello.csv").eachLine { line ->
+  new File("Hello.csv").eachLine { line ->
     println line}
 }
